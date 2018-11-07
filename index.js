e2ad788b8c68ac53fd0bc717a99f03fdf5719af0
Ethan <ethansagin@gmail.com>
2018-11-07 22:33:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendKitten(){
-  kittens.push()
+function destructivelyAppendKitten("Ralph"){
+  kittens.push("Ralph")
   return kittens
 }
-console.log(destructivelyAppendKitten("Ralph"))
